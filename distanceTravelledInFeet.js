3f9e87af1c881d6cbd45b57c9b007ad607079b51
ErnestMaina <you@example.com>
2023-09-19 15:43:59 +0300

    Updates index.js

---
+++ distanceTravelledInFeet.js
@@ -0,0 +1,11 @@
+function distanceTravelledInFeet(start, destination) {
+  //returns the number of feet traveled
+  if (destination - start) {
+    return (destination - start) * 264;
+  }
+
+  return destination - start;
+
+
+
+}
--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(somevalue){
+  if (somevalue < 42 ){
+    return 42- somevalue;
+
+  }
+return somevalue-42;
+
+}
+
+function distanceFromHqInFeet(someValue) {
+  const distance= distanceFromHqInBlocks(someValue);
+  return distance * 264;
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if (destination>start){
+    return (destination-start)*264;
+  }
+else if (start>destination){
+  return(start-destination)*264;
+}
+}
+
+
+function calculatesFarePrice(start, destination) {
+  let cost=0;
+
+  if (distanceTravelledInFeet(start, destination)<= 400){
+    cost=0;
+  }
+
+  else if(distanceTravelledInFeet(start, destination) <=2000 && distanceTravelledInFeet(start, destination) >400){
+    cost=(distanceTravelledInFeet(start, destination) -400) * 0.02;
+  }
+  else if(distanceTravelledInFeet(start, destination) >2000 && distanceTravelledInFeet(start, destination) <2500){
+    cost=25;
+  }
+  else if(distanceTravelledInFeet(start, destination)>2500){
+    cost='cannot travel that far';
+  }
+
+return cost;
+
+}
+
+
+
+
+
+
+
